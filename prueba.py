c60d3696c97c80605bfe78ce15ec78659c9616f3
SebaCendra <sebastiancendra@gmail.com>
2024-04-11 00:30:17 -0300

    funcion resta

--- prueba.py
+++ prueba.py
@@ -1,6 +1,11 @@
+def resta(x,y):
+    return y-x
+
 def main():
     x=5+5
     print(f"La suma de 5+5={x}")
+    y=int(input("Ingrese el valor de y:"))
+    print(f"La resta es:{resta(x,y)}")
     
 if __name__ == "__main__":
     main()
